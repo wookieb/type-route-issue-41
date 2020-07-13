91e0df8fe54d9273c816d5a84c27e917aea2987f
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2020-07-13 22:17:58 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {createRouter, defineRoute} from "type-route";
+
+const {routes, RouteProvider, useRoute} = createRouter({
+    home: defineRoute('/'),
+    login: defineRoute('/login')
+});
+
+function LoginPage() {
+    return <div>Login page</div>;
+}
+
+function HomePage() {
+    return <div>Home page</div>;
+}
+
+function requiresRedirectToLogin(route, isLoggedIn) {
+    return route.name === 'home' && !isLoggedIn;
+}
+
+function Routing({isLoggedIn}) {
+    const route = useRoute();
+
+    useEffect(() => {
+        if (requiresRedirectToLogin(route, isLoggedIn)) {
+            routes
+                .login()
+                .replace();
+        }
+    }, [route]);
+
+    if (requiresRedirectToLogin(route, isLoggedIn)) {
+        return <div>Redirecting...</div>;
+    }
+
+    if (route.name === 'home') {
+        return <HomePage />;
+    } else if (route.name === 'login') {
+        return <LoginPage />;
+    }
+    return <div>Not found</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <RouteProvider>
+        <Routing isLoggedIn={false}/>
+    </RouteProvider>;
+
 }
 
 export default App;
